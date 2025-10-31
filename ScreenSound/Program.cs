342af241aa0bae3d3615389523b533eb0f9bae29
Diogo <diogo.moraes@optdriven.com.br>
2025-10-31 15:50:08 -0300

    feat: melhorando o menu com o orm

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,38 +2,8 @@
 using ScreenSound.Modelos;
 using ScreenSound.Banco;
 
-try
-{
-    var context = new ScreenSoundContext();
-    //using, ser executados durante somente onde estao registrados, mais gerenciamento. boa prática para garantir que objetos sejam usados e descartados com segurança, mesmo se ocorrerem erros dentro do bloco.
-    var artistaDAL = new ArtistaDAL(context);
-
-    var novoArtista = new Artista("Luigi cabrini", "Gostoso") { Id = 3};
-    ;
-
-    artistaDAL.Atualizar(novoArtista);
-    artistaDAL.Deletar(novoArtista);
-
-
-    var ListaArtistas = artistaDAL.Listar();
-   
-    foreach(var artista in ListaArtistas)
-    {
-        Console.WriteLine(artista);
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-return;
-
-Artista ira = new Artista("Ira!", "Banda Ira!");
-Artista beatles = new("The Beatles", "Banda The Beatles");
-
-Dictionary<string, Artista> artistasRegistrados = new();
-artistasRegistrados.Add(ira.Nome, ira);
-artistasRegistrados.Add(beatles.Nome, beatles);
+var context = new ScreenSoundContext();
+var artistaDAL = new ArtistaDAL(context);
 
 Dictionary<int, Menu> opcoes = new();
 opcoes.Add(1, new MenuRegistrarArtista());
@@ -72,7 +42,7 @@ void ExibirOpcoesDoMenu()
     if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
     {
         Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
-        menuASerExibido.Executar(artistasRegistrados);
+        menuASerExibido.Executar(artistaDAL);
         if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
     } 
     else
